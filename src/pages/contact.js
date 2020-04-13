82f408ec1775b02a7f515d615089faccdf49d1b0
Levani's ART <levani.qotolashvili@gmail.com>
2020-04-13 23:19:44 +0400

    changed e-mail description

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const Contact = () => (
     <SEO title="Contact" />
     <h1 className="title px-0 col-md-6">If you have a project in mind, complete the form below to send your enquiry.</h1>
     <div className="contact col-md-8 px-0 mt-5">
-      <h2 className="email font-weight-normal mb-4">Email me at - <a href="mailto:levani.qotolashvili@gmail.com">levani.qotolashvili@gmail.com</a></h2>
+      <h2 className="email font-weight-normal mb-4">Email - <a href="mailto:levani.qotolashvili@gmail.com">levani.qotolashvili@gmail.com</a></h2>
       <form action="https://formspree.io/levani.qotolashvili@gmail.com" method="POST">
         <div className="d-flex flex-wrap contact-info">
           <div className="form-input col-lg-6 col-12 pr-0 pl-0 pr-lg-2">
